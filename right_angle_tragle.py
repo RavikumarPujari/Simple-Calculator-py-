986777bd1b3bc11f9e88a4675cc9e3e46a0e1ee7
Ravikumar Pujari <ravipujari2005@gmail.com>
2026-04-11 01:20:20 +0530

    Add files via upload

---
+++ right_angle_tragle.py
@@ -0,0 +1,8 @@
+#Using While loop to print the right angle trangle 
+no_of_row = int(input())     
+no_of_columns =int(input())
+
+counter = 0 
+while counter < no_of_row :
+    print("* " * counter  )
+    counter = counter + 1
